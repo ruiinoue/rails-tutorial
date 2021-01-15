af4bd76ef5d5aecad7e1a762c2305e5f33f52dc0
ruiinoue <jingshangrui422@gmail.com>
2021-01-15 17:14:04 +0900

    rootURLの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   # resources :microposts
   # resources :users
-  root 'application#hello'
+  # root 'application#hello'
 end
