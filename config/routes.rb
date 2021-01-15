540dc866590ec7a3df81583ac84aeefd18131951
ruiinoue <jingshangrui422@gmail.com>
2021-01-15 15:09:19 +0900

    hello worldのセット完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :microposts
-  resources :users
-  root 'users#index'
+  # resources :microposts
+  # resources :users
+  root 'application#hello'
 end
