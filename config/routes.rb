293cc940a530ca383ed36c9d8d08f219b5502b5f
ruiinoue <60529151+ruiinoue@users.noreply.github.com>
2021-01-21 13:41:43 +0900

    userコントローラーの追加 (#54)
    * userコントローラー追加の準備
    
    * userコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
   root 'static_pages#home'
   # get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  # resources :microposts
-  # resources :users
   # root 'application#hello'
 end
