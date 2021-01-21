6d1b031b38a3045df0f56d23323395e4aea2266e
ruiinoue <jingshangrui422@gmail.com>
2021-01-21 13:39:25 +0900

    userコントローラー追加の準備

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  # resources :microposts
-  # resources :users
   # root 'application#hello'
 end
