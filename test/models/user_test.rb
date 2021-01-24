be16320da7640abb2a6e827b79e6efc686388af2
ruiinoue <jingshangrui422@gmail.com>
2021-01-24 11:57:08 +0900

    不足部分を追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,6 +10,16 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
