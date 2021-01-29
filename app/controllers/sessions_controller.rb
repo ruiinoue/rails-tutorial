5cd8439f20c58a358024f0fc0a9cbe11d91b9b19
ruiinoue <jingshangrui422@gmail.com>
2021-01-29 16:00:16 +0900

    フラッシュメッセージの表示とテスト

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      
     else
-
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
