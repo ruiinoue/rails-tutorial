86b3256faf792c08bfea9a93f1507c6993180987
ruiinoue <60529151+ruiinoue@users.noreply.github.com>
2021-01-30 21:16:24 +0900

    remember_digestの演習 (#139)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,25 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
 
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
+    class << self
+        def digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+        end
+
+        def new_token
+            SecureRandom.urlsafe_base64
+        end
     end
 
-    def User.new_token
-        SecureRandom.urlsafe_base64
-    end
+    # def User.digest(string)
+    #     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    #     BCrypt::Password.create(string, cost: cost)
+    # end
+
+    # def User.new_token
+    #     SecureRandom.urlsafe_base64
+    # end
 
     def remember
         self.remember_token = User.new_token
