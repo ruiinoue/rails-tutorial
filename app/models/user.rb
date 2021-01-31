eaf8339652ec4c8636da11df11d748511bc4e81a
ruiinoue <jingshangrui422@gmail.com>
2021-01-31 10:34:55 +0900

    クッキーを削除する

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,15 +18,6 @@ class User < ApplicationRecord
         end
     end
 
-    # def User.digest(string)
-    #     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    #     BCrypt::Password.create(string, cost: cost)
-    # end
-
-    # def User.new_token
-    #     SecureRandom.urlsafe_base64
-    # end
-
     def remember
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
@@ -34,5 +25,9 @@ class User < ApplicationRecord
 
     def authenticated?(remember_token)
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
     end
 end
