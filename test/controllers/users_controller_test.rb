10a0acfe41c10beff8f822b1766f587f2f1ea1c5
ruiinoue <60529151+ruiinoue@users.noreply.github.com>
2021-02-01 20:06:29 +0900

    ユーザーの一覧ページ (#172)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "should get new" do
     get signup_url
     assert_response :success
     assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
 end
