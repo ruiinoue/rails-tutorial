26b173b0d5f24233f32a3da02b9d5cd937b167a3
ruiinoue <jingshangrui422@gmail.com>
2021-02-02 18:53:19 +0900

    adminユーザーのテストの演習

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,4 +17,28 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: {
+      user: {
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@user), params: {
+      user: {
+        password: "password",
+        password_confirmation: "password",
+        admin: true
+      }
+    }
+    assert_not @other_user.reload.admin?
+  end
 end
